09d794f35594c80bd9b7a9a71ab101c4ec18cd78
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-09-07 21:16:32 +0530

    Added the authRoutes in middlewares of server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import morgan from "morgan";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use("/api/auth", authRoutes);
+
 
 //Test route 
 app.get("/", (req,res) => {
